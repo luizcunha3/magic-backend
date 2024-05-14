f2a38f2be2238092d95a11feb8254861cf34537b
Luiz Carlos Cunha <luiz.cunha.3@gmail.com>
2024-05-14 00:13:53 -0300

    initialize express.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("chris :)")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/oi', (req, res) => {
+    let carta = {
+        "name": "Sol Ring",
+        "cost": "{2}"
+    }
+    res.send(carta)
+})
+
+//app.get(1param, 2parm)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+/**
+ * /cards/{idCarta} -> ...../cards/1234
+ */
