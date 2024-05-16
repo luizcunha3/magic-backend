21a292f99e4a98797f31a4cf1fa54fd72174144e
Luiz Carlos Cunha <luiz.cunha.3@gmail.com>
2024-05-15 22:36:02 -0300

    adds segunda.js

---
+++ segunda.js
@@ -0,0 +1,75 @@
+//Objetos
+let cartas = [
+    {
+        "nome": "Sol Ring",
+        "custo": {
+            "incolor": 2,
+            "branco": 0,
+            "verde": 0,
+            "azul": 0,
+            "vermelho": 0,
+            "preto": 0
+        },
+        "modificadores": [],
+        "texto": "vira e adiciona duas mana incolor",
+        "tipo": "artefato"
+    },
+    {
+        "nome": "Rhystic Studies",
+        "custo": {
+            "incolor": 1,
+            "branco": 0,
+            "verde": 0,
+            "azul": 1,
+            "vermelho": 0,
+            "preto": 0
+        },
+        "modificadores": [],
+        "texto": "Quando um oponente jogar um feitiço, você pode comprar um card, a não sert que aquele jogador pague 1",
+        "tipo": "Encantamento"
+    },
+    {
+        "nome": "Dockside Extortionist",
+        "custo": {
+            "incolor": 1,
+            "branco": 0,
+            "verde": 0,
+            "azul": 0,
+            "vermelho": 1,
+            "preto": 0
+        },
+        "modificadores": [],
+        "texto": "Quando Dockside Extortionist entra no campo de batalha, crie X tokens de tesouro, onde X é o numero de artefatos e encantamentos que seus oponentes controlam",
+        "tipo": ["Criatura", "Goblin", "Pirata"]
+    },
+    {
+        "nome": "Hostile Investigator",
+        "custo": {
+            "incolor": 3,
+            "branco": 0,
+            "verde": 0,
+            "azul": 0,
+            "vermelho": 0,
+            "preto": 1
+        },
+        "texto": "Quando Hostile Investigator entra no campo de batalha, oponente alvo descarta uma carta. Quando um ou mais jogadores discartam uma ou mais cartas, investigue. Essa habilidade só é ativada uma vez por turno",
+        "tipo": ["Criatura", "Ogro", "Ladino", "Detetive"]
+    },
+    {
+        "nome": "Rakdos, Patrono do Caos",
+        "custo": {
+            "incolor": 4,
+            "branco": 0,
+            "verde": 0,
+            "azul": 0,
+            "vermelho": 1,
+            "preto": 1
+        },
+        "modificadores": ["Voar", "Atropelar"],
+        "texto": "",
+        "tipo": ["Criatura", "Goblin", "Pirata"]
+    },
+
+]
+
+//Callbacks e closures
